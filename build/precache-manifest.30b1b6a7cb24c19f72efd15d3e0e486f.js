978527b0907c89c5a49965183828ecd7a1a7ac8d
Duy Pham <pham.hong.duy@valuence.inc>
2021-05-01 16:26:28 +0700

    update

---
+++ build/precache-manifest.30b1b6a7cb24c19f72efd15d3e0e486f.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "334b8d340c40abf6b409fe7caf2006ed",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fe9f2399701fcb8601e6",
+    "url": "/static/css/2.703a7081.chunk.css"
+  },
+  {
+    "revision": "d8051cb4dc7fbfb2659a",
+    "url": "/static/css/main.d727d864.chunk.css"
+  },
+  {
+    "revision": "fe9f2399701fcb8601e6",
+    "url": "/static/js/2.9e1ee9c2.chunk.js"
+  },
+  {
+    "revision": "114da400c9b16703f3f9f0595118eacf",
+    "url": "/static/js/2.9e1ee9c2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d8051cb4dc7fbfb2659a",
+    "url": "/static/js/main.ac81ac53.chunk.js"
+  },
+  {
+    "revision": "04bbf290f173f6d62ce4",
+    "url": "/static/js/runtime-main.b17252c3.js"
+  },
+  {
+    "revision": "688f92e32e4716633943de9c7bdef66e",
+    "url": "/static/media/avatar.688f92e3.svg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "82fa597f29de41cd41a7c402bcf09ba5",
+    "url": "/static/media/summernote.82fa597f.ttf"
+  },
+  {
+    "revision": "c1a96d26d30d9e0b2fd33c080d88c72e",
+    "url": "/static/media/summernote.c1a96d26.woff"
+  },
+  {
+    "revision": "f4a47ce92c02ef70fc848508f4cec94a",
+    "url": "/static/media/summernote.f4a47ce9.eot"
+  },
+  {
+    "revision": "f694db69cded200e4edd999fddef81b7",
+    "url": "/static/media/summernote.f694db69.woff2"
+  }
+]);
